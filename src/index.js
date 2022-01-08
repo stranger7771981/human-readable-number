3fbd2ead27573aa661a344bf0a44f1752a4dcf44
Denis Uglov <denisuglov1981@gmail.com>
2022-01-08 22:33:09 +0600

    add: task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function toReadable (number) {
-  
-}
+
+    let nums = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
+    let dec = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
+    
+        if (number < 20) return nums[number];
+        let dig = number%10;
+        if (number < 100) return dec[~~(number/10)-2] + (dig? " " + nums[dig]: "");
+        if (number < 1000) return nums[~~(number/100)] +" hundred" + (number%100 == 0? "": " " + toReadable(number%100));
+    }
